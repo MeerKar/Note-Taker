6a0630d37a51014aa0d246e764744b705c2a097d
MeerKar <meku.jithu@gmail.com>
2024-03-12 22:07:17 -0700

    added comments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
-// Defines the get request to this routes end point '/api/notes'
+//  Get request to  routes end point '/api/notes'
 router.get("/api/notes", async (req, res) => {
   const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
   res.json(dbJson);
 });
 
-// Defines the post request to this routes end point '/api/notes'
+// Post request to routes end point '/api/notes'
 router.post("/api/notes", (req, res) => {
   const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
   const newFeedback = {
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,11 @@
 const router = require("express").Router();
 const path = require("path");
 
-// Defines the route that sends 'index.html' as a response to a client when a GET request is made
+// Routes for 'index.html' as a response to a client
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-// Defines the route that sends 'notes.html" as a response to a client when a GET request is made
+//Routes for 'notes.html" as a response to a client
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
