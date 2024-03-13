1700ca6f34380c4396bf6e59db05d420b9659715
MeerKar <meku.jithu@gmail.com>
2024-03-12 22:11:55 -0700

    tested

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use(html_routes);
 app.use(api_routes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`App running on http://localhost:${PORT}`);
 });
